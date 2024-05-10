a250c79920b61c5c059e9ab878947b9502a62702
Preslav977 <p.cvetanow@gmail.com>
2024-05-10 10:31:11 +0300

    Created test to see if post is rendered in the nav component.

--- src/tests/NavComponent.test.jsx
+++ src/tests/NavComponent.test.jsx
@@ -1,20 +1,20 @@
-import { render, screen } from "@testing-library/react";
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
 import { RouterProvider, createMemoryRouter } from "react-router-dom";
 import { describe, expect, it } from "vitest";
 import userEvent from "@testing-library/user-event";
 import NavComponent from "../components/NavComponent";
 import App from "../App";
+import routes from "../router/routes";
 
 describe("Should render NavComponent", () => {
   it("should render NavComponent", () => {
-    const routes = [
-      {
-        path: "/",
-        element: <NavComponent />,
-      },
-    ];
-
-    const router = createMemoryRouter(routes, {});
+    const router = createMemoryRouter(routes, {
+      initialEntries: ["/"],
+    });
 
     render(<RouterProvider router={router} />);
 
@@ -26,14 +26,9 @@ describe("Should render NavComponent", () => {
   });
 
   it("should render NavComponent navigation links", async () => {
-    const routes = [
-      {
-        path: "/",
-        element: <NavComponent />,
-      },
-    ];
-
-    const router = createMemoryRouter(routes, {});
+    const router = createMemoryRouter(routes, {
+      initialEntries: ["/"],
+    });
 
     render(<RouterProvider router={router} />);
 
@@ -52,5 +47,29 @@ describe("Should render NavComponent", () => {
     expect(screen.queryByText("Traditions").textContent).toMatch(/traditions/i);
     expect(screen.queryByText("Customs").textContent).toMatch(/customs/i);
     expect(screen.queryByText("History").textContent).toMatch(/history/i);
+  });
+
+  it("should render post besides navigation links", async () => {
+    const router = createMemoryRouter(routes, {
+      initialEntries: ["/"],
+    });
+
+    render(<RouterProvider router={router} />);
+
+    const apiLoading = screen.queryByTestId("loading");
+
+    expect(apiLoading).toBeInTheDocument();
+
+    await waitForElementToBeRemoved(() => screen.queryByTestId("loading"));
+
+    screen.debug();
+
+    const postCategory = screen.queryByText("folkore (myths and legends)");
+
+    expect(postCategory).toBeInTheDocument();
+
+    expect(
+      screen.queryByText("Bulgaria - Myths And Legends").textContent,
+    ).toMatch(/bulgaria - myths And legends/i);
   });
 });
