2905b0d9fafdc4be487453458eea7b12d7a376b3
Preslav977 <p.cvetanow@gmail.com>
2024-05-20 13:47:25 +0300

    Refreshing the page without preventing default behaviour is causing an issue of not changing the state of the button. I am going to research using fetch with POST.

--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,62 @@ export const PostContext = createContext(null);
 
 export const UserContext = createContext(null);
 
+export const EmailContext = createContext(null);
+
+export const UsernameContext = createContext(null);
+
+export const FirstNameContext = createContext(null);
+
+export const LastNameContext = createContext(null);
+
+export const PasswordContext = createContext(null);
+
+export const ConfirmPasswordContext = createContext(null);
+
 function App() {
   const [posts, setPosts] = useState([]);
 
-  const [user, setUser] = useState({});
+  const [userObject, setUserObject] = useState({
+    email: "",
+    username: "",
+    first_name: "",
+    last_name: "",
+    password: "",
+    confirm_password: "",
+  });
+
+  const [email, setEmail] = useState("");
+
+  const [username, setUsername] = useState("");
+
+  const [firstName, setFirstName] = useState("");
+
+  const [lastName, setLastName] = useState("");
+
+  const [password, setPassword] = useState("");
+
+  const [confirmPassword, setConfirmPassword] = useState("");
 
   return (
     <>
       <PostContext.Provider value={[posts, setPosts]}>
-        <UserContext.Provider value={{ user, setUser }}>
-          <NavComponent />
-          <Outlet />
+        <NavComponent />
+        <UserContext.Provider value={{ userObject, setUserObject }}>
+          <EmailContext.Provider value={{ email, setEmail }}>
+            <UsernameContext.Provider value={{ username, setUsername }}>
+              <FirstNameContext.Provider value={{ firstName, setFirstName }}>
+                <LastNameContext.Provider value={{ lastName, setLastName }}>
+                  <PasswordContext.Provider value={{ password, setPassword }}>
+                    <ConfirmPasswordContext.Provider
+                      value={{ confirmPassword, setConfirmPassword }}
+                    >
+                      <Outlet />
+                    </ConfirmPasswordContext.Provider>
+                  </PasswordContext.Provider>
+                </LastNameContext.Provider>
+              </FirstNameContext.Provider>
+            </UsernameContext.Provider>
+          </EmailContext.Provider>
         </UserContext.Provider>
       </PostContext.Provider>
 
--- src/components/SignUpFormComponent.jsx
+++ src/components/SignUpFormComponent.jsx
@@ -1,13 +1,65 @@
 import styles from "./SignUpFormComponent.module.css";
 import { Link } from "react-router-dom";
-import { useContext } from "react";
-import { UserContext } from "../App";
+import { useContext, useState } from "react";
+import {
+  UserContext,
+  EmailContext,
+  UsernameContext,
+  FirstNameContext,
+  LastNameContext,
+  PasswordContext,
+  ConfirmPasswordContext,
+} from "../App";
 
 function SignUpFormComponent() {
-  const { user, setUser } = useContext(UserContext);
+  const { userObject, setUserObject } = useContext(UserContext);
+
+  const { email, setEmail } = useContext(EmailContext);
+
+  const { username, setUsername } = useContext(UsernameContext);
+
+  const { firstName, setFirstName } = useContext(FirstNameContext);
+
+  const { lastName, setLastName } = useContext(LastNameContext);
+
+  const { password, setPassword } = useContext(PasswordContext);
+
+  const { confirmPassword, setConfirmPassword } = useContext(
+    ConfirmPasswordContext,
+  );
+
+  const [disableSignUpBtn, setDisableSignUpBtn] = useState(true);
+
+  // eslint-disable-next-line no-useless-escape
+  const emailRegex = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
+
+  function enableButtonIfNoErrors() {
+    if (email.match(emailRegex)) {
+      setDisableSignUpBtn(false);
+    }
+  }
 
   function handleSubmit(e) {
     e.preventDefault();
+
+    const FormDataObject = new FormData(e.target);
+    const userEmail = FormDataObject.get("email");
+    const userUsername = FormDataObject.get("username");
+    const userFirstName = FormDataObject.get("first_name");
+    const userLastName = FormDataObject.get("last_name");
+    const userPassword = FormDataObject.get("password");
+    const userConfirmPassword = FormDataObject.get("confirm_password");
+
+    const createUser = {
+      ...userObject,
+      email: userEmail,
+      username: userUsername,
+      first_name: userFirstName,
+      last_name: userLastName,
+      password: userPassword,
+      confirm_password: userConfirmPassword,
+    };
+    setUserObject(createUser);
   }
 
   return (
@@ -35,7 +87,14 @@ function SignUpFormComponent() {
               minLength={5}
               maxLength={30}
               required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
+            {!email.match(emailRegex) && (
+              <span className={styles.error}>
+                Email does not match required format
+              </span>
+            )}
           </div>
           <div className={styles.formContentWrapper}>
             <label htmlFor="username">Username:</label>
@@ -45,7 +104,14 @@ function SignUpFormComponent() {
               minLength={5}
               maxLength={30}
               required
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
             />
+            {username.length < 5 && (
+              <span className={styles.error}>
+                Username must be between 5 and 30 characters.
+              </span>
+            )}
           </div>
           <div className={styles.formContentWrapper}>
             <label htmlFor="first_name">First Name:</label>
@@ -55,7 +121,14 @@ function SignUpFormComponent() {
               minLength={5}
               maxLength={30}
               required
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
             />
+            {firstName.length < 5 && (
+              <span className={styles.error}>
+                First Name must be between 5 and 30 characters.
+              </span>
+            )}
           </div>
           <div className={styles.formContentWrapper}>
             <label htmlFor="last_name">Last Name:</label>
@@ -65,20 +138,55 @@ function SignUpFormComponent() {
               minLength={5}
               maxLength={30}
               required
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
             />
+            {lastName.length < 5 && (
+              <span className={styles.error}>
+                Last Name must be between 5 and 30 characters.
+              </span>
+            )}
           </div>
           <div className={styles.formContentWrapper}>
             <label htmlFor="password">Password:</label>
-            <input type="password" name="password" minLength={8} required />
+            <input
+              type="password"
+              name="password"
+              minLength={8}
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            {password.length < 8 && (
+              <span className={styles.error}>
+                Password must be at least 8 characters long.
+              </span>
+            )}
           </div>
           <div className={styles.formContentWrapper}>
             <label htmlFor="confirm_password">Confirm Password:</label>
-            <input type="password" name="confirm_password" />
+            <input
+              type="password"
+              name="confirm_password"
+              required
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+            {password !== confirmPassword && (
+              <span className={styles.error}>Passwords don&apos;t match</span>
+            )}
           </div>
           <div className={styles.signUpButtonContainer}>
-            <button className={styles.signUpButton} type="submit">
-              <Link to="/account/login">Sign Up</Link>
-            </button>
+            <Link to="/account/login">
+              <button
+                // disabled={disableSignUpBtn}
+                // onClick={enableButtonIfNoErrors}
+                className={styles.signUpButton}
+                type="submit"
+              >
+                Sign Up
+              </button>
+            </Link>
           </div>
           <p data-testid="signUpFormTextAndLink">
             Already have an account?{" "}
