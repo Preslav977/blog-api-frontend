4380e1817eb3c7ed302989116474ca391a83e4f3
Preslav977 <p.cvetanow@gmail.com>
2024-06-01 14:42:01 +0300

    Created a test to render a span on the log in form component.

--- src/tests/LogInFormComponent.test.jsx
+++ src/tests/LogInFormComponent.test.jsx
@@ -24,6 +24,45 @@ describe("should render LogInFormComponent", () => {
 
     expect(screen.queryByText("Email:").textContent).toMatch(/email:/i);
 
+    expect(
+      screen.queryByText("Email does not match required format").textContent,
+    ).toMatch(/email does not match required format/i);
+
+    expect(screen.queryByText("Password:").textContent).toMatch(/password:/i);
+
+    expect(
+      screen.queryByText("Password must be at least 8 characters long.")
+        .textContent,
+    ).toMatch(/password must be at least 8 characters long./i);
+
+    expect(screen.queryByTestId("logInFormTextAndLink").textContent).toEqual(
+      "Don't an account yet? Sign Up Now",
+    );
+
+    const logInBtn = screen.queryAllByRole("button");
+
+    expect(logInBtn[1].textContent).toMatch(/log in/i);
+  });
+
+  it("should render the content and the span errors.", () => {
+    const router = createMemoryRouter(routes, {
+      initialEntries: ["/", "/account/login"],
+    });
+
+    render(<RouterProvider router={router} />);
+
+    screen.debug();
+
+    expect(
+      screen.queryByText("Welcome back to Bulgarian!").textContent,
+    ).toMatch(/welcome back to bulgarian!/i);
+
+    expect(screen.queryByTestId("logInFormPrivacy").textContent).toEqual(
+      "By continuing, you are agree to our User Agreement and Privacy Policy.",
+    );
+
+    expect(screen.queryByText("Email:").textContent).toMatch(/email:/i);
+
     expect(screen.queryByText("Password:").textContent).toMatch(/password:/i);
 
     expect(screen.queryByTestId("logInFormTextAndLink").textContent).toEqual(
